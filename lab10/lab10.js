c3ef78437853ee2becbf342e9a73c675c17ea1d5
Orobs <50989230+bryan0x17@users.noreply.github.com>
2021-07-28 15:44:20 -0600

    Finish HTML and CSS

---
+++ lab10/lab10.js
@@ -0,0 +1,3 @@
+const restaurantInspectionAPI = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json';
+const ratInspectionAPI = 'https://data.cityofnewyork.us/resource/p937-wjvj.json';
+const cafeteriaInspectionAPI = 'https://data.cityofnewyork.us/resource/9hxz-c2kj.json';
